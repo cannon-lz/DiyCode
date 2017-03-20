b470d0c1bec7924056c376e9db5332952486029f
zhangluya <zhangluya@zhangluya.vcredit.cn>
2017-03-20 18:41:56 +0800

    增加评论实体

---
+++ app/src/main/java/com/zly/diycode/main/Reply.java
@@ -0,0 +1,15 @@
+package com.zly.diycode.main;
+
+import com.zly.diycode.R;
+import com.zly.diycode.common.adapter.Item;
+
+/**
+ * Created by zhangluya on 2017/3/20.
+ */
+
+public class Reply implements Item {
+    @Override
+    public int getItemViewType() {
+        return R.layout.item_reply;
+    }
+}
